0fd4805709d17ed3cf2bb7c2742b53fe08298d0a
Skwal98 <levkalex98@gmail.com>
2022-01-08 23:12:27 +0300

    first part implementation warning angular worm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,75 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
   ValidationErrors,
   ValidatorFn,
 } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { WarningMessageType } from './warning-form-info/warning-form-info.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'FormWarning';
 
   profileForm = this._fb.group({
-    firstName: this._fb.control('', this.forbiddenNameValidator()),
-    lastName: [''],
+    firstName: this._fb.control('', [
+      this.forbiddenNameValidator(),
+      this.warningValidator(this.digitNameValidator()),
+    ]),
+    lastName: this._fb.control('', [
+      this.forbiddenNameValidator(),
+      this.digitNameValidator(),
+    ]),
   });
 
-  public get validFirstNameControl(): boolean {
+  public get validForm(): boolean {
     return this.profileForm.valid;
   }
 
-  constructor(private _fb: FormBuilder) {
+  constructor(private _fb: FormBuilder, private _cd: ChangeDetectorRef) {
     setTimeout(() => {
       console.log(this.profileForm);
     }, 500);
   }
 
   forbiddenNameValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value === '1234') {
+        return { forbiddenName: { value: 'invalidFormat' } };
+      }
+      return null;
+    };
+  }
+
+  digitNameValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (control.value === '123456789') {
+        return { diginName: { value: 'digitFormat' } };
+      }
+      return null;
+    };
+  }
+
+  warningValidator(validatorFn: ValidatorFn): ValidatorFn {
     const warningFn = (
       control: AbstractControlWarning
     ): ValidationErrors | null => {
-      if (control.value === '1') {
-        control.warnings = { forbiddenName: { value: 'invalid data' } };
+      control.warnings ??= new BehaviorSubject<WarningMessageType>(null);
+      var result = validatorFn(control);
+      if (result !== null) {
+        control.warnings.next(result);
       } else {
-        control.warnings = null;
+        control.warnings.next(control.warnings.value);
       }
       return null;
     };
@@ -45,7 +78,7 @@ export class AppComponent {
 }
 
 export interface AbstractControlWarning extends AbstractControl {
-  warnings: any;
+  warnings: BehaviorSubject<WarningMessageType>;
 }
 
 export interface WarningFn {
--- src/app/warning-form-info/warning-form-info.component.ts
+++ src/app/warning-form-info/warning-form-info.component.ts
@@ -1,25 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ControlContainer, FormGroup } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { AbstractControl, ControlContainer } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { AbstractControlWarning } from '../app.component';
 
 @Component({
   selector: 'app-warning-form-info',
   templateUrl: './warning-form-info.component.html',
   styleUrls: ['./warning-form-info.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WarningFormInfoComponent implements OnInit {
+export class WarningFormInfoComponent {
   @Input() controlName!: string;
 
   constructor(private _formContainer: ControlContainer) {}
 
-  public get warning(): string {
-    const warnings = (
-      this._formContainer.control?.get(
-        this.controlName
-      ) as AbstractControlWarning
-    )?.warnings;
-    return JSON.stringify(warnings);
+  public get warning(): Observable<WarningMessageType> {
+    const warnings$ = (this._control as AbstractControlWarning)?.warnings;
+    return warnings$?.asObservable();
   }
 
-  ngOnInit(): void {}
+  public warningsMessages(obj: WarningMessageType): string[] {
+    if (obj) {
+      return Object.keys(obj).map<string>(
+        (errorName) => `${errorName}.${obj[errorName].value}`
+      );
+    }
+    return [];
+  }
+
+  public get errorMessages(): string[] | null {
+    const errors = this._control?.errors;
+    if (errors) {
+      return Object.keys(errors).map<string>(
+        (errorName) => `${errorName}.${errors[errorName].value}`
+      );
+    }
+    return null;
+  }
+
+  private get _control(): AbstractControl | null | undefined {
+    return this._formContainer.control?.get(this.controlName);
+  }
 }
+
+export type WarningMessageType = { [key: string]: any } | null;
